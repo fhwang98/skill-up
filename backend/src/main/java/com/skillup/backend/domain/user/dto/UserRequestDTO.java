8f0a5fef5d0617a422bd4e29ca89c765b19fdce0
fhwang98 <fhwang9815@gmail.com>
2025-12-09 18:30:17 +0900

    feat: 회원가입시 UserRequestDTO 유효성 검증 Spring Validation 코드 추가

--- backend/src/main/java/com/skillup/backend/domain/user/dto/UserRequestDTO.java
+++ backend/src/main/java/com/skillup/backend/domain/user/dto/UserRequestDTO.java
@@ -1,5 +1,9 @@
 package com.skillup.backend.domain.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,9 +13,15 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UserRequestDTO {
 
+    @Email(message = "이메일 형식이 아닙니다.")
+    @NotBlank(message = "이메일을 입력해주세요.")
     private String email;
 
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*]{8,16}$", message = "비밀번호는 8~16자의 영문 대소문자, 숫자, 특수문자로 이루어져야 합니다.")
     private String password;
 
+    @NotBlank(message = "닉네임을 입력해주세요.")
+    @Size(min = 2, max = 10, message = "닉네임은 2~10자로 입력해주세요.")
     private String nickname;
 }
