42d24eb0c085febde405a8cbb3e17660636b3166
fhwang98 <fhwang9815@gmail.com>
2025-12-09 19:43:49 +0900

    docs: swagger api 문서 어노테이션 추가

--- backend/src/main/java/com/skillup/backend/domain/user/api/UserController.java
+++ backend/src/main/java/com/skillup/backend/domain/user/api/UserController.java
@@ -2,7 +2,13 @@ package com.skillup.backend.domain.user.api;
 
 import com.skillup.backend.domain.user.dto.UserRequestDTO;
 import com.skillup.backend.domain.user.service.UserService;
-import com.skillup.backend.global.common.ApiResponse;
+import com.skillup.backend.global.common.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -17,18 +23,25 @@ import java.util.Map;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
+@Tag(name = "USER API", description = "회원 API")
 public class UserController {
 
     private final UserService userService;
 
     // 자체 회원가입 api
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ApiResponse<Map<String, Long>>> createUser(@Validated @RequestBody UserRequestDTO dto) {
+    @Operation(summary = "자체 회원가입", description = "회원가입 api")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "회원가입 성공", content = @Content(schema = @Schema(implementation = BaseResponse.class))),
+            @ApiResponse(responseCode = "400", description = "유효성 검증 실패", content = @Content(schema = @Schema(implementation = BaseResponse.class))),
+            @ApiResponse(responseCode = "409", description = "이메일 중복", content = @Content(schema = @Schema(implementation = BaseResponse.class)))
+    })
+    public ResponseEntity<BaseResponse<Map<String, Long>>> createUser(@Validated @RequestBody UserRequestDTO dto) {
 
         log.info("회원가입 api 요청 POST /users");
         Long id = userService.createUser(dto);
         Map<String, Long> responseBody = Collections.singletonMap("userId", id);
-        return ResponseEntity.status(201).body(ApiResponse.success(responseBody));
+        return ResponseEntity.status(201).body(BaseResponse.success(responseBody));
 
     }
 
