1e38c0b01366db4b3dd2b6d37f55bb0b8f18d678
fhwang98 <fhwang9815@gmail.com>
2025-12-10 21:52:16 +0900

    feat: UserRequestDTO 재사용 위한 그룹화

--- backend/src/main/java/com/skillup/backend/domain/user/dto/UserRequestDTO.java
+++ backend/src/main/java/com/skillup/backend/domain/user/dto/UserRequestDTO.java
@@ -13,15 +13,19 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UserRequestDTO {
 
+    public interface existEmailGroup {} // 회원 가입시 이메일 중복 검사
+    public interface existNicknameGroup {} // 회원 가입시 이메일 중복 검사
+    public interface createGroup {} // 회원 가입시
+
     @Email(message = "이메일 형식이 아닙니다.")
-    @NotBlank(message = "이메일을 입력해주세요.")
+    @NotBlank(message = "이메일을 입력해주세요.", groups = {createGroup.class, existEmailGroup.class})
     private String email;
 
-    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @NotBlank(message = "비밀번호를 입력해주세요.", groups = {createGroup.class})
     @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*]{8,16}$", message = "비밀번호는 8~16자의 영문 대소문자, 숫자, 특수문자로 이루어져야 합니다.")
     private String password;
 
-    @NotBlank(message = "닉네임을 입력해주세요.")
+    @NotBlank(message = "닉네임을 입력해주세요.", groups = {createGroup.class, existNicknameGroup.class})
     @Size(min = 2, max = 10, message = "닉네임은 2~10자로 입력해주세요.")
     private String nickname;
 }
