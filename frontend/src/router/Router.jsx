d7318e36cbb2efc9c3316a7f6048e16994352a31
fhwang98 <fhwang9815@gmail.com>
2025-12-11 17:32:58 +0900

    feat: 회원가입 페이지 라우터 추가

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -1,4 +1,5 @@
 import App from "@/App";
+import JoinPage from "@/pages/JoinPage";
 import MainPage from "@/pages/MainPage";
 import { createBrowserRouter } from "react-router-dom";
 
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
 			{
 				path: "",
 				element: <MainPage />,
+			},
+			{
+				path: "/join",
+				element: <JoinPage />,
 			},
 		],
 	},
