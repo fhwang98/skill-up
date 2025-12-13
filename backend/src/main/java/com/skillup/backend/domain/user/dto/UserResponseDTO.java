5af616e7b8a2601c34022bac369e3d9959ba5c76
fhwang98 <fhwang9815@gmail.com>
2025-12-13 20:32:37 +0900

    feat: 회원정보 조회시 반환할 정보 UserResponseDTO 생성
    - 이메일, 닉네임, 가입날짜, 수정날짜
    - 가입날짜, 수정날짜 가져오기 위해 BaseEntity에 Getter 추가

---
+++ backend/src/main/java/com/skillup/backend/domain/user/dto/UserResponseDTO.java
@@ -0,0 +1,15 @@
+package com.skillup.backend.domain.user.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class UserResponseDTO {
+    private String email;
+    private String nickname;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
