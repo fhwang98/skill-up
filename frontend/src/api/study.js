b570bd46b7e7b38469d00059ec7dfb3ee54e6f7a
fhwang98 <fhwang9815@gmail.com>
2025-12-19 12:42:44 +0900

    feat: 프론트엔드 스터디관련 api 호출 코드
    - createStudy 스터디 생성
    - getStudies 스터디 목록 조회
    - getTags 스터디 태그 조회

---
+++ frontend/src/api/study.js
@@ -0,0 +1,64 @@
+import fetchWithAccess from "@/utils/fetchUtil";
+
+const BASE_URL = import.meta.env.VITE_BACKEND_API_BASE_URL;
+
+export const createStudy = async ({
+	title,
+	description,
+	maxMembers,
+	recruitEndDate,
+	startDate,
+	category,
+	endDate,
+}) => {
+	const res = await fetchWithAccess(`${BASE_URL}/studies`, {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({
+			title,
+			description,
+			maxMembers,
+			recruitEndDate,
+			startDate,
+			category,
+			endDate,
+		}),
+	});
+
+	if (!res) {
+		throw new Error("서버와 통신할 수 없습니다.");
+	}
+
+	return await res.json();
+};
+
+export const getStudies = async ({ page, keyword }) => {
+	const params = new URLSearchParams({
+		page,
+		keyword,
+	});
+	const res = await fetch(`${BASE_URL}/studies?${params.toString()}`, {
+		method: "GET",
+	});
+
+	if (!res) {
+		throw new Error("서버와 통신할 수 없습니다.");
+	}
+
+	return await res.json();
+};
+
+export const getTags = async (keyword) => {
+	const params = new URLSearchParams({
+		keyword,
+	});
+	const res = await fetch(`${BASE_URL}/tags?${params.toString()}`, {
+		method: "GET",
+	});
+
+	if (!res) {
+		throw new Error("서버와 통신할 수 없습니다.");
+	}
+
+	return await res.json();
+};
